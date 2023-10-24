64219c291b7be86d8035133e2bcf18d2de13281a
hochianhkhoi2003 <khoi.hota1602@hcmut.edu.vn>
2023-10-24 21:48:28 +0700

    init for Ex6

--- LAB2_PROJECT/Core/Src/main.c
+++ LAB2_PROJECT/Core/Src/main.c
@@ -56,7 +56,20 @@ static void MX_TIM2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;
+void setTimer0 ( int duration ){
+  timer0_counter = duration / TIMER_CYCLE ;
+  timer0_flag = 0;
+}
 
+void timer_run (){
+ if( timer0_counter > 0){
+	 timer0_counter --;
+	 if( timer0_counter == 0) timer0_flag = 1;
+ }
+}
 /* USER CODE END 0 */
 
 /**
@@ -94,13 +107,13 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+  setTimer0 (1000) ;
+  while (1) {
+	if( timer0_flag == 1){
+		HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin );
+		setTimer0 (2000) ;
+	}
   }
-  /* USER CODE END 3 */
 }
 
 /**
@@ -208,14 +221,9 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-int counter = 100;
 void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim )
 	{
-	counter --;
-	if( counter <= 0){
-		counter = 100;
-		HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin );
-	}
+	timer_run ();
 }
 /* USER CODE END 4 */
 
