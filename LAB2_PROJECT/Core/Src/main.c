bfce4ad356795f5b0aae58b54beec0a38583a169
hochianhkhoi2003 <khoi.hota1602@hcmut.edu.vn>
2023-10-26 18:23:46 +0700

    Update Ex5

--- LAB2_PROJECT/Core/Src/main.c
+++ LAB2_PROJECT/Core/Src/main.c
@@ -50,15 +50,22 @@ TIM_HandleTypeDef htim2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
-void updateClockBuffer();
-int hour = 15, minute = 8, second = 50;
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int hour = 15, minute = 8, second = 50;
+const int MAX_LED = 4;
+int index_led = 0;
+int led_buffer [4] = {1, 2, 3, 4};
+void updateClockBuffer() {
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute / 10;
+	led_buffer[3] = minute % 10;
+}
 /* USER CODE END 0 */
 
 /**
@@ -114,15 +121,6 @@ int main(void)
   }
 }
 
-const int MAX_LED = 4;
-int index_led = 0;
-int led_buffer [4] = {1, 2, 3, 4};
-void updateClockBuffer() {
-	led_buffer[0] = hour / 10;
-	led_buffer[1] = hour % 10;
-	led_buffer[2] = minute / 10;
-	led_buffer[3] = minute % 10;
-}
 /**
   * @brief System Clock Configuration
   * @retval None
